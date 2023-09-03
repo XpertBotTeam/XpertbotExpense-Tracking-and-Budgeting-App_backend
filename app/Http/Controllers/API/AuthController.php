7e139f92b609d1025e3a219d706c465a3553783c
mahdiabouzeid <mahdiabouzeid602@gmail.com>
2023-09-03 13:13:50 +0300

    fixing API

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use JWTAuth;
+
+
+
+class AuthController extends Controller
+{
+
+public function login(Request $request){
+        $credentials=$request->only('email','password');
+        $credentials= [ 
+            'email'=>$request->email,
+            'password'=>$request->password
+        ];
+        if(Auth::attempt($credentials)){
+            
+            $user=Auth::user();
+           $access_token = $user->createToken('authtoken')->plainTextToken;
+           return response()->json([
+                'status'=>true,
+                'token'=>$access_token,
+                'message'=>'User logged-in successfully'
+            ]);
+        }
+        else{
+            return response()->json([
+            'status'=>false,
+            'message'=>'wrong username or password '
+            ]);
+        }
+    }
+
+    
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['status'=>'false','message'=>'user already exists']);
+        }
+
+        else{
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json(['status'=>true,'message' => 'User registered successfully', 'token' => $token]);
+        }
+    }
+
+}
+
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,18 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+//use Laravel\Passport\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens as SanctumHasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+
+    use HasApiTokens,Notifiable;
 
     protected $fillable = ['name', 'email', 'password'];
-
+    protected $primaryKey='id';
+    protected $hidden=['password'];
    
     public function expenses()
     {
