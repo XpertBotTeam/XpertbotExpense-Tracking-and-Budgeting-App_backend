f10e88765cefaf28cc96c1d17e2339b15536b996
mahdiabouzeid <mahdiabouzeid602@gmail.com>
2023-09-03 13:27:03 +0300

    Fixing TYPO

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\API\authcontroller;
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('login',[authcontroller::class,'login']);
-Route::post('register',[authcontroller::class,'register']);
+Route::post('login',[AuthController::class,'login']);
+Route::post('register',[AuthController::class,'register']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
